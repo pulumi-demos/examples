99c3f75075b4c15a752be6637f64e7d74b4b9b4d
MitchellGerdisch <mitch@pulumi.com>
2023-02-10 09:47:20 -0600

    added component and other things

--- python/aws-eks-base-infra-py/__main__.py
+++ python/aws-eks-base-infra-py/__main__.py
@@ -1,30 +1,44 @@
-import iam
-import vpc
-import utils
 import pulumi
 from pulumi_aws import eks
+
+import iam
+import utils
+
+import network
+
+# Get stack-specific config
+config = pulumi.Config()
+service_name = config.get("service_name") or pulumi.get_project()
+desired_size = config.get_int("desired_count") or 2
+max_size = config.get_int("max_count") or 2
+min_size = config.get_int("min_count") or 1
+
+network=network.Vpc(f'{service_name}-net', network.VpcArgs())
+subnet_ids=[]
+for subnet in network.subnets:
+    subnet_ids.append(subnet.id)
 
 ## EKS Cluster
 
 eks_cluster = eks.Cluster(
-    'eks-cluster',
+    f'{service_name}-cluster',
     role_arn=iam.eks_role.arn,
     tags={
         'Name': 'pulumi-eks-cluster',
     },
     vpc_config=eks.ClusterVpcConfigArgs(
         public_access_cidrs=['0.0.0.0/0'],
-        security_group_ids=[vpc.eks_security_group.id],
-        subnet_ids=vpc.subnet_ids,
+        security_group_ids=[network.fe_security_group.id],
+        subnet_ids=subnet_ids,
     ),
 )
 
 eks_node_group = eks.NodeGroup(
-    'eks-node-group',
+    f'{service_name}-nodegroup',
     cluster_name=eks_cluster.name,
     node_group_name='pulumi-eks-nodegroup',
     node_role_arn=iam.ec2_role.arn,
-    subnet_ids=vpc.subnet_ids,
+    subnet_ids=subnet_ids,
     tags={
         'Name': 'pulumi-cluster-nodeGroup',
     },
