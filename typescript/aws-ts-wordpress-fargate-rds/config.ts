8164e55ffd50da1994d7fe94a7399fc879cda80a
MitchellGerdisch <mitch@pulumi.com>
2023-04-05 11:09:55 -0500

    initial commit

---
+++ typescript/aws-ts-wordpress-fargate-rds/config.ts
@@ -0,0 +1,25 @@
+import { Config, getStack } from "@pulumi/pulumi";
+import { RandomPassword } from "@pulumi/random";
+
+const config = new Config();
+
+// name base for naming conventions
+export const nameBase = config.get("nameBase") || `eksbase-${getStack()}`;
+
+// Get db info
+export const dbName = config.get("dbName") || "backend";
+export const dbUser = config.get("dbUser") || "admin";
+// Get secretified password from config or create one using the "random" package
+export let dbPassword = config.getSecret("dbPassword");
+if (!dbPassword) {
+  dbPassword = new RandomPassword("dbPassword", {
+    length: 16,
+    special: true,
+    overrideSpecial: "_%",
+  }).result;
+}
+
+// stack tags used to group stacks in the service.
+export const stackTagName = config.get("stackTagName") ?? "Application";
+export const stackTagValue = config.get("stackTagValue") ?? "Guestbook";
+
