f39f4c72b88b1dbb00c2f43427fad70b7f1e79eb
MitchellGerdisch <mitch@pulumi.com>
2023-07-16 15:36:03 -0500

    stack config input support added

--- python/automation-api/automation.py
+++ python/automation-api/automation.py
@@ -9,10 +9,20 @@ arrangement_name = args.arrangement
 org = args.org
 stack = args.stack
 destroy = args.destroy
+config_params = args.config
 arrangements_file = "./arrangements.yaml"
 
+# Build config map based on any config parameters passed on the command line.
+config = []
+for config_param in config_params:
+    config_name_value = config_param.split("=")
+    config.append({
+        "name":config_name_value[0],
+        "value":config_name_value[1]
+    })
+
 # The Pulumi projects are up two levels.
-run_automation("../..", arrangements_file, arrangement_name, org, stack, destroy)
+run_automation("../..", arrangements_file, arrangement_name, org, stack, destroy, config)
 
 
 
